cfc052e24767ec0a3f768544428dcd2c832a7041
Madeleine Woodbury <mail@woodburycode.com>
2025-03-30 10:58:41 -0400

    Add satellite movement and collision handling scripts; use shatter in Rosetta collision scene

--- Assets/Scripts/CubeSatScenes/CollisionHandler.cs
+++ Assets/Scripts/CubeSatScenes/CollisionHandler.cs
@@ -3,15 +3,10 @@ using UnityEngine;
 
 /// <summary>
 /// Handles collision detection for the satellite.
-/// When a collision with debris is detected, it triggers the detonation of the satellite's bomb.
+/// When a collision with debris is detected, it activates the fragments.
 /// </summary>
 public class CollisionHandler : MonoBehaviour
 {
-    /// <summary>
-    /// Reference to the SatelliteExplosion script on the parent object.
-    /// </summary>
-    // private SatelliteExplosion explosionScript;
-    // private FragmentMovement fragmentMovementScript;
     [SerializeField] private GameObject originalObject;
     [SerializeField] private GameObject fragmentObject;
     private bool collisionDetected = false;
@@ -22,11 +17,6 @@ public class CollisionHandler : MonoBehaviour
     /// </summary>
     void Start()
     {
-        // Find the parent object and get the SatelliteExplosion script
-        // explosionScript = GetComponentInParent<SatelliteExplosion>();
-
-        // Find the parent object and get the FragmentMovement script
-        // fragmentMovementScript = GetComponentInParent<FragmentMovement>();
 
         if(fragmentObject != null)
         {
@@ -42,31 +32,26 @@ public class CollisionHandler : MonoBehaviour
 
     /// <summary>
     /// Called when the satellite collides with another object.
-    /// If the object is tagged as "Debris", it triggers the detonation of the satellite's bomb.
     /// </summary>
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision detected with: " + collision.gameObject.name);
         if(!collisionDetected)
         {
+            Debug.Log("Collision detected with: " + collision.gameObject.name);
             ActivateFragments();
-        }
-        // if(collision.gameObject.tag == "Debris" || collision.gameObject.tag == "Satellite")
-        // {
-        //     if(explosionScript != null)
-        //     {
-        //         // Trigger the detonation of the bomb
-        //         explosionScript?.DetonateBomb(); 
-        //     }
 
-        //     if(fragmentMovementScript != null)
-        //     {
-        //         // Apply velocity to fragments
-        //         fragmentMovementScript?.ApplyVelocityToFragments();
-        //     }
-        // }
+            // If the fragment object has a MoveSatellite script, start moving it
+            MoveSatellite moveScript = gameObject.GetComponent<MoveSatellite>();
+            if (moveScript != null)
+            {
+                moveScript.StartMoving();
+            }
+        }
     }
 
+    /// <summary>
+    /// Activates the fragments and disables the original object.
+    /// </summary>
     public void ActivateFragments()
     {
         if (originalObject != null && fragmentObject != null)
