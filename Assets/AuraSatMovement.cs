edfff26481bf4a3c046cb2006c618f6e54e64b90
Ingrid L <ijo022@uit.no>
2025-03-30 12:41:42 +0200

    Movement satellite almost complete, angles needs some work still

--- Assets/AuraSatMovement.cs
+++ Assets/AuraSatMovement.cs
@@ -2,22 +2,22 @@ using UnityEngine;
 
 // Script for movement of debris orbiting planet 
 
-public class DebrisMovementAuraSatScene : MonoBehaviour
+public class AuraSatMovement : MonoBehaviour
 {
     // Reference to planet object
     [SerializeField] private Transform planet;
 
     // Distance from planet
-    [SerializeField] private float orbitRadius = 100f; 
+    [SerializeField] private float orbitRadius = 106f;
 
-    [SerializeField] private float moveSpeed = 5f;   
+    [SerializeField] private float moveSpeed = 5f;
 
-    [SerializeField] private float startAngle = 0f;  
+    [SerializeField] private float startAngle = 0f;
 
     // Angle trajectory
-    [SerializeField] private float angleRange = 90f; 
+    [SerializeField] private float angleRange = 90f;
 
-    private float currentAngle; 
+    private float currentAngle;
 
     private void Start()
     {
@@ -40,7 +40,6 @@ public class DebrisMovementAuraSatScene : MonoBehaviour
         float x = planet.position.x + Mathf.Cos(angleRad) * orbitRadius;
         float z = planet.position.z + Mathf.Sin(angleRad) * orbitRadius;
 
-        // Apply new position
         transform.position = new Vector3(x, transform.position.y, z);
     }
 }
